24355a02833a3e8f5350daad32a6475c3ad9e459
Mahin Malhotra <mahinmalhotra20@gmail.com>
2020-06-01 07:46:04 +0530

    added working list tags

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
+def sample_user(email = 'mahin@test.com', password= 'testest'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email,password)
+
+# def sample_user(email='test@londonappdev.com', password='testpass'):
+#     """Create a sample user"""
+#     return get_user_model().objects.create_user(email, password)
 
 
 
@@ -40,3 +48,16 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+
+        tag = models.Tag.objects.create(
+        user = sample_user(),
+        name = 'Vegan'
+        )
+
+
+        self.assertEqual(str(tag),tag.name)
